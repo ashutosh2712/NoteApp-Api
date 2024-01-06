42c118f116e2854887ad64c82d14b5cb45e29ed3
Ashutosh Kumar Roy <ashu00619@gmail.com>
2024-01-06 11:58:25 +0530

    Note model created ,DRF install and first url register.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Note(models.Model):
+    body = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
